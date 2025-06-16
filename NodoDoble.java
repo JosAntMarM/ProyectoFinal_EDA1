51c60daccdf5a120be8687dc1334f824acdbf72a
Jose Antonio <jmarquezmallqui@gmail.com>
2025-06-16 16:21:57 -0500

    Create NodoDoble.java

---
+++ NodoDoble.java
@@ -0,0 +1,12 @@
+public class NodoDoble {
+    int clave;
+    NodoDoble next;
+    NodoDoble prev;
+
+    public NodoDoble(int clave) {
+        this.clave = clave;
+        this.next = null;
+        this.prev = null;
+    }
+    
+}
